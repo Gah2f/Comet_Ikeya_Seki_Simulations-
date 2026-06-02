1775f1fb746a40bdcb102a7001f48456dab0da5b
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2026-06-02 16:00:26 -0400

    Sodium Einstein Coff vs Wave_length

---
+++ Codes/Spectral/Na.py
@@ -0,0 +1,84 @@
+from astropy.table import Table
+from astropy import units as u
+from astropy.constants import c, h
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = Table.read('Spectral/Na(2).csv')
+
+
+
+# print(t.colnames)
+# print(t[:5])
+# nt= np.array([
+#     float(x.replace('""', 'NAN').replace('"', '').replace('=', ''))
+#     for x in t['obs_wl_vac(nm)'].data
+# ]) * u.nm
+
+# # wave_lengthv= np.array([])
+# print(nt)
+
+def clean_numeric_column(col):
+    out = []
+    for x in col:
+        s = x.replace('"', '').replace('=', '').strip()
+        if s == '' or s.lower() == 'nan':
+            out.append(np.nan)
+            continue
+        try:
+            out.append(float(s))
+        except:
+            out.append(np.nan)
+    return np.array(out)
+
+def clean_J_column(col):
+    out = []
+    
+    for x in col:
+        s = x.replace('"', '').replace('=', '').strip()
+        if s == '' or s.lower() == 'nan':
+            out.append(np.nan)
+            continue
+        if '/' in s:
+            try:
+                num, denom = s.split('/')
+                out.append(float(num) / float(denom))
+            except:
+                out.append(np.nan)
+            continue
+        try:
+            out.append(float(s))
+        except:
+            out.append(np.nan)    
+    return np.array(out)
+
+wave_lengthv = clean_numeric_column(t['obs_wl_vac(nm)'])
+A21 = clean_numeric_column(t['Aki(s^-1)']) * 1e8 / u.s
+Ji = clean_J_column(t['J_i'])
+Jk = clean_J_column(t['J_k'])
+
+mask = (
+    np.isfinite(wave_lengthv) &
+    np.isfinite(A21) &
+    np.isfinite(Ji) &
+    np.isfinite(Jk)
+)
+
+wave_lengthv = wave_lengthv[mask]
+A21 = A21[mask]
+Ji = Ji[mask]
+Jk = Jk[mask]
+
+lam = wave_lengthv * u.nm
+wi = 2 * Ji + 1
+wk = 2 * Jk + 1
+
+B12 = (wk / wi) * A21 * lam.to(u.m) ** 5 / (8 * np.pi * h * c)
+
+plt.figure()
+plt.scatter(lam.value, B12.value, s=5)
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('Einstein B Coefficient (m^3 s^-2)')
+plt.title('Einstein B Coefficient for Sodium Atom Transitions')
+plt.grid(True)
+plt.show()
---
+++ Codes/Spectral/Sodium_atom.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy import units as u
+from astropy.constants import c, h
+
+lam = np.array([
+     2852.81, 2853.01,
+    3302.37, 3302.98,
+    5889.950, 5895.924,
+    8183.256, 8194.790,
+    8194.824, 11381.45,
+    11403.78, 18465.25
+]) * u.angstrom
+
+A21 = np.array([
+    0.00554,
+    0.00554,
+    0.0281,
+    0.0281,
+    0.616,
+    0.614,
+    0.453,
+    0.090,
+    0.540,
+    0.090,
+    0.176,
+    0.140
+]) * 1e8 / u.s
+
+J_lower = np.array([
+    0.5, 0.5,
+    0.5, 0.5,
+    0.5, 0.5,
+    0.5, 1.5,
+    1.5, 0.5,
+    1.5, 2.5
+])
+
+J_upper = np.array([
+    1.5, 0.5,
+    1.5, 0.5,
+    1.5, 0.5,
+    1.5, 1.5,
+    2.5, 0.5,
+    0.5, 3.5
+])
+
+w1 = 2 * J_lower + 1
+w2 = 2 * J_upper + 1
+
+B12 = (w2 / w1) * A21 * lam.to(u.m) ** 5 / (8 * np.pi * h * c)
+
+plt.figure()
+plt.scatter(lam.value, B12.value)
+plt.xlabel('Wavelength (Angstrom)')
+plt.ylabel('Einstein B Coefficient (m^3 s^-2)')
+plt.title('Einstein B Coefficient for Sodium Atom Transitions')
+
+plt.grid(True)
+plt.show()
